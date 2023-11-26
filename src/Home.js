e6aa3b6a49631b389be85d3f031cf59813836d35
promdiGamer <al_jager1983@yahoo.com>
2023-11-26 13:00:34 +0800

    lesson 19-error handling

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-
+    const [error, setError] = useState(null);
    
 
     //handles the delete of blogs, passed as props
@@ -16,15 +16,24 @@ const Home = () => {
     useEffect(() => {
       setTimeout(() => {
         console.log("use effect ran");
-      fetch('http://localhost:8000/blogs')
+      fetch('http://localhost:8000/blogds')
       .then(res => {
+        if(!res.ok){        //error throwing
+          throw Error('could not fetch the data for that resource');
+        }
         return  res.json();
       })
       .then(data => {
         console.log(data);
         setBlogs(data);
         setIsPending(false);
+        setError(null);
       })
+      .catch(err =>{              //catching error, e.g. network error
+        // console.log(err.message);
+        setIsPending(false);
+        setError(err.message);
+      } )   
       }, 1000);
       
     }, []); //dependecy array
@@ -32,6 +41,7 @@ const Home = () => {
     return ( 
     <div className="home">
       {/* {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}  --> for lesson 17*/}
+      { error &&  <div>{error}</div> }
       {isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
     </div>
