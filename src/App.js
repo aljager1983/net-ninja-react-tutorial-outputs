350eee43fde2b66385c43a7721f85c3d21c2b54f
promdiGamer <al_jager1983@yahoo.com>
2023-11-27 22:04:03 +0800

    lesson25-router parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './Home';
 import Navbar from './Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create  from './Create';
+import BlogDetails from './BlogDetails';
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/create" element={<Create />}/>
+          <Route path="/blogs/:id" element={<BlogDetails />}/>
         </Routes>
       </div>
     </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title}) => {  //destructured, same as below
 // const BlogList = ({blogs, title, handleDelete}) => {  //destructured, same as below
 // const BlogList = (props) => {
@@ -8,9 +10,11 @@ const BlogList = ({blogs, title}) => {  //destructured, same as below
             <h2>{title}</h2>
              {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
+            <Link to={`/blogs/${blog.id}`}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author} </p>
             {/* <button onClick={() => handleDelete(blog.id)}>Delete</button> */}
+            </Link>
         </div>
        ))}
         </div>
